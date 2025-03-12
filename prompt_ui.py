a8525aadc8a30e5ffb8e64842a83d14fe7dd21c5
Sunilsangfroid <sunilsangfroid@gmail.com>
2025-03-12 11:24:42 +0530

    Implement chatbot functionality and update prompt handling;

--- prompt_ui.py
+++ prompt_ui.py
@@ -38,13 +38,13 @@ length_input = st.selectbox("Select Explanation Length",["Short (1-2 paragraphs)
 
 template = load_prompt('template.json')
 
-# Fill the Placeholder
-prompt = template.invoke({
-    'paper_input':paper_input,
-    'style_input':style_input,
-    'length_input':length_input
-})
 
 if st.button('Summarize'):
-    result = model.invoke(prompt)
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    # Fill the Placeholder
     st.write(result.content)
